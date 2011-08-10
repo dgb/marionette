34b757291cdfe113d497253be7693507676a513a
Derek Barnes <derek@bigbento.com>
2011-08-10 00:22:36 -0700

    Move from old project

---
+++ README.md
@@ -0,0 +1,30 @@
+Marionette
+================================================================================
+A Lightweight Client-side Application Framework
+--------------------------------------------------------------------------------
+
+Marionette is inspired by HTTP and Sinatra.
+
+Many Javascript frameworks provide a heavy framework on the client that causes duplication in code.
+
+Marionette eschews this, and instead has the client behave like its namesake. The idea is: we've already written this application for the server, so why not simply have the browser behave accordingly? Essentially, we design the client-side application so that its dispatch cycle mimics what would happen in a page-based, server side system, but taking advantage of lighter data-based payloads.
+
+We do this by:
+
+* Binding views to URLs rather than an intermediate data object.
+* Making a request with every dispatch.
+* Use HTTP caching so that those requests are completed quickly.
+* Using event delegation to dispatch anchor clicks to the appropriate view.
+* Completely redrawing the view with every dispatch.
+
+In this way, you can utilize the existing server-side application in order to drive the client-side one. Indeed, with technology like Mustache, you can share even templates between the two.
+
+Marionette will not depend on any external libraries. It's intended to be used "invisibly" with another library that you might use in an application that refreshes every page.
+
+Right now, Marionette serves as more of a proof of concept than anything else. Here are some things I need to improve:
+
+* Cross browser compatibility. This is caused by not having dependencies on any particular library.
+* Ensure compatibility with popular client side libraries.
+* Make a Rack middleware that detects incoming headers and modifies them appropriately (i.e. the Location header)
+* Use Link headers instead of customer headers for templates
+* Tests, dummy.
---
+++ marionette.js
@@ -0,0 +1,103 @@
+/* Things Marionette takes for granted
+
+XMLHttpRequest (sorry IE6)
+JSON.parse
+addEventListener (sorry IE)
+
+*/
+
+var Marionette = {};
+
+Marionette.Application = function(templatePath){
+	/*
+		requestUrl
+		requestRunning
+		requestResponseData
+		templates
+	*/
+	
+	this.onfailure = function(e){};
+	
+	var self = this;
+	
+	var onSuccess = function(){
+		if(this.readyState == 1){
+			self.requestRunning = true;
+		}
+		if(this.readyState == 4){
+			self.requestRunning = false;
+			if(this.status == 200){
+				var redirect = this.getResponseHeader('X-Marionette-Location');
+				if(redirect){
+					this.open('GET', redirect);
+					this.setXHRHeaders();
+					this.send();
+				}else{
+					var json;
+					try{
+						json = JSON.parse(this.responseText);
+					}catch(e){
+						self.onfailure.call(self, e);
+						return;
+					}
+					self.requestResponseData = json;
+					
+					// Todo: Use Link headers instead of custom ones
+					var templateName = this.getResponseHeader('X-Marionette-Template');
+					var layout = this.getResponseHeader('X-Marionette-Layout') || 'layout';
+
+					var rendered = Mustache.to_html(self.templates[templateName], json);
+					var laid = Mustache.to_html(self.templates[layout], {"yield":rendered})
+					
+					var container = document.getElementById('container').innerHTML = laid;
+				}
+			}
+		}
+	}
+	
+	this.xhr = new XMLHttpRequest();
+	
+	this.xhr.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			// Set up the normal callback
+			this.onreadystatechange = onSuccess;
+			
+			// Set up the listeners
+			// what do we do about assholes who click-click-click-click?
+			document.addEventListener('click', function(evt){
+				if(evt.target.tagName == 'A'){
+					evt.preventDefault();
+					self.dispatch(evt.target);
+				}
+			});
+			document.addEventListener('submit', function(evt){
+				evt.preventDefault();
+				self.dispatch(evt.target);
+			});
+			
+			// TODO: error handling
+			self.templates = JSON.parse(this.responseText);
+		}else{
+			// Throw a "Can't get Templates" Exception
+		}
+	}
+	
+	this.xhr.open('GET', templatePath);
+	this.xhr.send();
+
+};
+
+Marionette.Application.prototype.dispatch = function(element){
+	if(this.requestRunning) this.xhr.abort();
+	var method = element.method || 'GET';
+	var url = element.action || element.href; // what about data-method?
+	this.requestUrl = url;
+	this.xhr.open(method, url);
+	this.setXHRHeaders();
+	this.xhr.send();
+}
+
+Marionette.Application.prototype.setXHRHeaders = function(){
+	this.xhr.setRequestHeader('Accept', 'application/json;');
+	this.xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+}
